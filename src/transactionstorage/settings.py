ec997e212a9640013ae3b69393cc4c3f20c18cb5
Alex Porozov <alex@getpatch.com>
2021-08-15 22:03:33 +0500

    remove dj secret from settings

--- src/transactionstorage/settings.py
+++ src/transactionstorage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(miy)!jpd45cp^0^g#(8eijq5_ek=fg68@gj4c@8lnx&hdmb)3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
