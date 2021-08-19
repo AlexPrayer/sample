d3e3803af0179c2b6ba0d7c389717d332bac16c2
Alex Porozov <alex@getpatch.com>
2021-08-20 02:04:19 +0500

    drf fmw notation

--- src/transactionstorage/storage/views.py
+++ src/transactionstorage/storage/views.py
@@ -1,33 +1,43 @@
 from django.db.models import Sum
 from rest_framework import serializers, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from transactionstorage.storage.models import Transaction
+from transactionstorage.storage.models import Transaction, TRANSACTION_TYPES
 
 
-class AggregatedAmountSerializer(serializers.Serializer):
-    total_amount = serializers.DecimalField(max_digits=12, decimal_places=2)
+class RequestSerializer(serializers.Serializer):
+    date_start = serializers.DateTimeField()
+    date_end = serializers.DateTimeField()
+    user_id = serializers.IntegerField()
+    transaction_type = serializers.ChoiceField(zip(TRANSACTION_TYPES, TRANSACTION_TYPES))
+
+
+class ReponseSerializer(serializers.Serializer):
+    total_amount = serializers.DecimalField(max_digits=12, decimal_places=2, allow_null=True)
+
+    def validate_total_amount(self, value):
+        if value is None:
+            raise ValidationError("No data for such query params")
+        return value
 
 
 class TransactionView(APIView):
-    serializer_class = AggregatedAmountSerializer
+    serializer_class = ReponseSerializer
+    request_serializer_class = RequestSerializer
     queryset = Transaction.objects.all()
 
     def get(self, request):
-        date_start = request.query_params.get('date_start')
-        date_end = request.query_params.get('date_end')
-        user_id = request.query_params.get('user_id')
-        transaction_type = request.query_params.get('type')
-        if not all(x is not None for x in [date_start, date_end, user_id, transaction_type]):
-            result = self.queryset.query.set_empty()
-        else:
-            result = self.queryset.filter(
-                user_id=user_id,
-                transaction_datetime__gte=date_start,
-                transaction_datetime__lte=date_end,
-                type=transaction_type
-            ).aggregate(total_amount=Sum('amount'))
+        request_serializer = RequestSerializer(request.query_params)
+        request_data = request_serializer.data
+
+        result = self.queryset.filter(
+            user_id=request_data["user_id"],
+            transaction_datetime__gte=request_data["date_start"],
+            transaction_datetime__lte=request_data["date_end"],
+            type=request_data["transaction_type"]
+        ).aggregate(total_amount=Sum('amount'))
 
         serializer = self.serializer_class(data=result)
         if serializer.is_valid():
